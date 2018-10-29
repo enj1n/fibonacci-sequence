39426ff06406a60b0f5314e888fa70c697bb6d46
Maria Nikandrova <enj1n@mail.ru>
2018-10-29 08:48:56 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
 //First method with For statement
-
-
-var x1=0;
-var x2=1;
-var x3;
-var number=prompt("Enter a limitation");
-document.write(x1+"<br/>");
-document.write(x2+"<br/>");
-for (var i=3; i<number; i++)
-{ x3=x1+x2;
-x1=x2;
-x2=x3;
-
-document.write(x3+<br/>);
+var a, b, result;
+a = 0;
+b = 1;
+result = b;
+for (var i = 1; i < 100; i++) {
+    document.write(result + "<br/>");
+    result = a + b;
+    a = b;
+    b = result;
 }
+
+
